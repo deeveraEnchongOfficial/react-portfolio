274ae602d58f28f7c0801895c74db688d06e38b5
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2023-02-07 12:10:55 +0800

    Adding Develop

---
+++ src/components/Skills.js
@@ -0,0 +1,89 @@
+// https://github.com/simple-icons/simple-icons/blob/master/slugs.md
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import colorSharp from "../assets/img/color-sharp.png";
+import constant from "../constant.json";
+
+export const Skills = () => {
+  console.log("constant", constant);
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.<br></br> Lorem Ipsum has been the industry's standard
+                dummy text.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                {constant.skills.map((item) => (
+                  <li key={item.name}>
+                    {item.title ? (
+                      <div key={item.length} className="item">
+                      <img src={item.image} />
+                          <h5>{item.title}</h5>
+                      </div>
+                    ) : (
+                      <p>Skills is Empty</p>
+                    )}
+                  </li>
+                ))}
+                {/* {constant.skills.map((skill, index) =>
+                  skill.length > 0 ? <p>True</p> : <p>False{skill.title}</p>
+                )} */}
+                {/* <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Logo Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                            <img src="https://cdn.simpleicons.org/JavaScript" />
+                                <h5>JavaScript</h5>
+                            </div> */}
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  );
+};
